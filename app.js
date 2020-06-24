380231b4d190300f578cfbcd04d2d5659dad0e7d
Sithu Kyaw <sithukyaw27500@gmail.com>
2020-06-24 18:58:07 +0630

    added configuration

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"))
-}).listen(3000)
+app
+  .get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  })
+  .listen(3000);
